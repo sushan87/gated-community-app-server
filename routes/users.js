35effbcd7dd871a8b4aad0d906a4eeea573b0396
sushan87 <Sushan.Chakraborty@gmail.com>
2025-03-07 12:11:40 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User'); // Import your User Mongoose model
+const mongoose = require('mongoose');
 
+// Routes
+
+// GET all users
+router.get('/getAllUsers', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// GET a single user by ID
+router.get('/getUserById/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// POST a new user
+router.post('/createUser', async (req, res) => {
+
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+  });
+
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+
+// DELETE an user by ID
+router.put('/deleteUserById/:id', async (req, res) => {
+
+  try {
+    const user = await User.findByIdAndDelete(new mongoose.Types.ObjectId(req.params.id.toString()));
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json({ message: 'User deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
