e1f5df5a5b66ba1d03fb62fad380f891a8a4d327
sushan87 <Sushan.Chakraborty@gmail.com>
2025-03-07 12:11:05 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
+
+// Define a Schema and Model
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+module.exports = mongoose.model('User', userSchema);
 
